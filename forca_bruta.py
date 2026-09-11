c612dd434457d807ed75d0ab2a26dfa4310a5970
João Victor Macêdo <joaovictornqm@gmail.com>
2026-09-11 19:29:18 -0300

    feat: adição de código.

--- forca_bruta.py
+++ forca_bruta.py
@@ -5,3 +5,13 @@ linhas = file.read().splitlines()
 
 coordenadas = {}
 pontos_de_entrega = []
+
+for n in range(int(i)):
+    line = linhas[n].split()
+    for m in line:
+        if m != "0":
+            coordenadas[m] = (n, line.index(m))
+            pontos_de_entrega.append(m)
+
+pontos_de_entrega.remove("R")
+menor_custo = float("inf")
